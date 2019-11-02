7cf954e779b2cf7660e5e1afb238f9387c88159f
Mr-Mason <hello_mason@163.com>
2019-11-02 20:23:04 +0800

    2019.11.12

--- WisdomTrip/app/src/main/java/com/example/wisdomtrip/activity/MainActivity.kt
+++ WisdomTrip/app/src/main/java/com/example/wisdomtrip/activity/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.wisdomtrip.activity
 
 import android.Manifest
+import android.annotation.TargetApi
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.wisdomtrip.R
 import com.example.wisdomtrip.activity.map.MainMapActivity
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +23,32 @@ class MainActivity : AppCompatActivity() {
         const val WRITE_COARSE_LOCATION_REQUEST_CODE = 200
     }
 
+    @TargetApi(Build.VERSION_CODES.P)
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // 延伸显示区域到刘海
+        val lp = window.attributes
+        lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        window.attributes = lp
+        // 设置页面全屏显示
+        val decorView = window.decorView
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//        setContentView(R.layout.activity_main)
         initPermissions()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            var decorView = window.decorView
+            decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
     }
 
     private fun initPermissions() {
@@ -37,6 +65,7 @@ class MainActivity : AppCompatActivity() {
             )
         } else {
             startActivity(Intent(this@MainActivity, MainMapActivity::class.java))
+            finish()
         }
 
     }
@@ -48,7 +77,7 @@ class MainActivity : AppCompatActivity() {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     startActivity(Intent(this@MainActivity, MainMapActivity::class.java))
                 } else {
-                    Toast.makeText(this,"定位权限没有开启，无法使用地图应用",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "定位权限没有开启，无法使用地图应用", Toast.LENGTH_SHORT).show()
                     finish()
                 }
             }
